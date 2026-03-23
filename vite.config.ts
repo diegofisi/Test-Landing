7dc488020a497de4114a3ca2d447b844713790f3
Diego Vicuña <39018790+diegofisi@users.noreply.github.com>
2026-03-23 18:53:18 -0500

    fix gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: './',
+  base: '/Test-Landing/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
